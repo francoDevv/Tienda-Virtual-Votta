65fb32cf376a05e05d50d5a77cb2157023a0b6f0
francoDevv <votta.franco16@gmail.com>
2022-10-23 19:40:46 -0300

    Pruebas 1

--- js/script.js
+++ js/script.js
@@ -1,87 +1,102 @@
-let saved_answer = "si"
-let user_answer = prompt("¿Deseas navegar en el sitio web Tienda Capricho Oficial? \nEscribe si para continuar");
+// let saved_answer = "si"
+// let user_answer = prompt("¿Deseas navegar en el sitio web Tienda Capricho Oficial? \nEscribe si para continuar");
 
-if (user_answer === saved_answer) {
-    alert("Bienvenido/a esperamos que encuentres lo que estas buscando, gracias por confiar en nosotros");
-} else {
-        alert("Nos veremos la proxima")
-}
+// if (user_answer === saved_answer) {
+//     alert("Bienvenido/a esperamos que encuentres lo que estas buscando, gracias por confiar en nosotros");
+// } else {
+//         alert("Nos veremos la proxima")
+// }
 
-class Product {
-    constructor(name, gender, color, size, price, payment_methods, id){
-        this.name = name;
-        this.gender = gender;
-        this.color = color;
-        this.size = size;
-        this.price = parseInt(price);
-        this.payment_methods = payment_methods;
-        this.id = id;
-    }
-    giveId(array){
-        this.id = array.length;
-    }
-}
+// class Product {
+//     constructor(name, gender, color, size, price, payment_methods, id){
+//         this.name = name;
+//         this.gender = gender;
+//         this.color = color;
+//         this.size = size;
+//         this.price = parseInt(price);
+//         this.payment_methods = payment_methods;
+//         this.id = id;
+//     }
+//     giveId(array){
+//         this.id = array.length;
+//     }
+// }
 
-const products = [
-    new Product("Jean", "Men", "Blue", "S - M - L - XL - XXl", 1000, "All payments methods", 1),
-    new Product("Poulover", "Women", "Blue and grey / Brown", "S - M - L - XL - XXl", 100, "All payments methods", 2),
-    new Product("Hoodie", "Men", "Beige / LigthBlue", "S - M - L - XL - XXl", 1300, "All payments methods", 3),
-    new Product("Denim Jacket", "Women", "LigthBlue", "S - M - L - XL - XXl", 900, "All payments methods", 4),
-    new Product("Sweater", "Boys", "Moss Green", "4, 6, 8, 10, 12, 14, 16", 1970, "All payments methods", 5),
-    new Product("Pijama", "Unisex", "Cocodrile / Walrus", "4, 6, 8, 10, 12, 14, 16", 300, "All payments methods", 6),
-]
+// const products = [
+//     new Product("Jean", "Men", "Blue", "S - M - L - XL - XXl", 1000, "All payments methods", 1),
+//     new Product("Poulover", "Women", "Blue and grey / Brown", "S - M - L - XL - XXl", 100, "All payments methods", 2),
+//     new Product("Hoodie", "Men", "Beige / LigthBlue", "S - M - L - XL - XXl", 1300, "All payments methods", 3),
+//     new Product("Denim Jacket", "Women", "LigthBlue", "S - M - L - XL - XXl", 900, "All payments methods", 4),
+//     new Product("Sweater", "Boys", "Moss Green", "4, 6, 8, 10, 12, 14, 16", 1970, "All payments methods", 5),
+//     new Product("Pijama", "Unisex", "Cocodrile / Walrus", "4, 6, 8, 10, 12, 14, 16", 300, "All payments methods", 6),
+// ]
 
-alert("Estos son los productos que tenemos en stock:\n" + JSON.stringify(products, null ,4));
+// alert("Estos son los productos que tenemos en stock:\n" + JSON.stringify(products, null ,4));
 
-let next = true;
+// let next = true;
 
-while (next) {
-    let add = prompt("Te gustaria que agregaramos un producto? Si es así, deja aquí lo siguientes datos (separados con un guion -): name - gender - color - size - price - payments methods. Ingresa la palabra NO para finalizar");
-    if (add.toUpperCase() == "NO") {
-        next = false;
-        break;
-    }
+// while (next) {
+//     let add = prompt("Te gustaria que agregaramos un producto? Si es así, deja aquí lo siguientes datos (separados con un guion -): name - gender - color - size - price - payments methods. Ingresa la palabra NO para finalizar");
+//     if (add.toUpperCase() == "NO") {
+//         next = false;
+//         break;
+//     }
 
-    let added_products = add.split("-");
-    console.log(added_products);
-    const user_products = new Product(added_products[0], added_products[1], added_products[2], added_products[3], added_products[4], added_products[5]);
-    products.push(user_products);
-    user_products.giveId(products);
-    console.log(products);
-}
+//     let added_products = add.split("-");
+//     console.log(added_products);
+//     const user_products = new Product(added_products[0], added_products[1], added_products[2], added_products[3], added_products[4], added_products[5]);
+//     products.push(user_products);
+//     user_products.giveId(products);
+//     console.log(products);
+// }
 
-let organize = prompt("Elegi la forma de ordenar los productos \n1) Nombre (A - Z) \n2) Nombre (Z - A) \n3) Precio ascendente \n4) Precio Descendente");
+// let organize = prompt("Elegi la forma de ordenar los productos \n1) Nombre (A - Z) \n2) Nombre (Z - A) \n3) Precio ascendente \n4) Precio Descendente");
 
-function ordenar(organize, array) {
-    let ordenado = array.slice(0);
+// function ordenar(organize, array) {
+//     let ordenado = array.slice(0);
 
-    switch (organize) {
-        case "1":
-            let nombre_ascendente = ordenado.sort((a,b)=>a.name.localeCompare(b.name));
-            return nombre_ascendente;
-        case "2":
-            let nombre_descendente = ordenado.sort((a,b)=>b.name.localeCompare(a.name));
-            return nombre_descendente;
-        case "3":
-            return ordenado.sort((a,b)=>a.price- b.price);
-        case "4":
-            return ordenado.sort((a,b)=> b.price - a.price);
-        default:
-            alert("No es un criterio valido para ordenar")
-            break;
-    }
-}
+//     switch (organize) {
+//         case "1":
+//             let nombre_ascendente = ordenado.sort((a,b)=>a.name.localeCompare(b.name));
+//             return nombre_ascendente;
+//         case "2":
+//             let nombre_descendente = ordenado.sort((a,b)=>b.name.localeCompare(a.name));
+//             return nombre_descendente;
+//         case "3":
+//             return ordenado.sort((a,b)=>a.price- b.price);
+//         case "4":
+//             return ordenado.sort((a,b)=> b.price - a.price);
+//         default:
+//             alert("No es un criterio valido para ordenar")
+//             break;
+//     }
+// }
 
-function string_result(array){
-    let info = "";
+// function string_result(array){
+//     let info = "";
 
-    array.forEach(elemento=>{
-        info += "Nombre: " + elemento.name + "\nGenero: " + elemento.gender + "\nColor: " + elemento.color + "\nSize: " + elemento.size + "\nPrecio: " + elemento.price + "\nMetodos de Pago: " + elemento.payment_methods + "\n\n"
-    })
+//     array.forEach(elemento=>{
+//         info += "Nombre: " + elemento.name + "\nGenero: " + elemento.gender + "\nColor: " + elemento.color + "\nSize: " + elemento.size + "\nPrecio: " + elemento.price + "\nMetodos de Pago: " + elemento.payment_methods + "\n\n"
+//     })
 
-    return info;
-}
+//     return info;
+// }
 
-alert(string_result(ordenar(organize, products)));
+// alert(string_result(ordenar(organize, products)));
 
-alert("Hasta luego");
+// alert("Hasta luego");
+
+const contenedorProductos = getElemementById("container-productos")
+
+stockPoductos.forEach((productos) => {
+    const contenedor = document.createElement("div")
+    div.classList.add("productos")
+    div.innerHTML = 
+        `<img src=${productos.img} alt="buzo crema">
+                <h5 class="card-title">${productos.nombre}</h5>
+                <h5 class="card-title">${productos.precio}</h5>
+                <p class="card-text card_fuente">${productos.desc}</p>
+                <p> Talle : ${productos.talle}<p>
+                <a href="#" id=agregar${productos.id} class="btn colorButton" >Agregar al carrito</a>`
+    contenedorProductos.appendChild(div)
+});
---
+++ js/stock.js
@@ -0,0 +1,14 @@
+let stockProductos = [
+    {id: 1, nombre: "Buzo 1", tipo: "buzo", cantidad: 1, desc: "Un buzo que re va con vos", precio: 1200, talle: "L", img: '../assets/images/buzoCremaNena.jpg'},
+    {id: 2, nombre: "Buzo 2", tipo: "buzo", cantidad: 1, desc: "Un buzo que re va con vos", precio: 1100, talle: "L", img: "../assets/images/buzoGrisNene.jpg"},
+    {id: 3, nombre: "Buzo 3", tipo: "buzo", cantidad: 1, desc: "Un buzo que re va con vos", precio: 1200, talle: "M", img: '../assets/images/buzoHombre.jpg'},
+    {id: 4, nombre: "Buzo 4", tipo: "buzo", cantidad: 1, desc: "Un buzo que re va con vos", precio: 1400, talle: "M", img: '../assets/images/buzoHombreCeleste.jpg'},
+    {id: 5, nombre: "Buzo 5", tipo: "buzo", cantidad: 1, desc: "Un buzo que re va con vos", precio: 1200, talle: "S", img: '../assets/images/buzoMujer.jpg'},
+    {id: 6, nombre: "Buzo 6", tipo: "buzo", cantidad: 1, desc: "Un buzo que re va con vos", precio: 1500, talle: "S", img: '../assets/images/buzoRosaMujer.jpg'},
+    {id: 7, nombre: "Remera 1", tipo: "remera", cantidad: 1, desc: "Una remera que re va con vos", precio: 500, talle: "L", img: '../assets/images/camisetaRayadaNene.jpg'},
+    {id: 8, nombre: "Remera 2", tipo: "remera", cantidad: 1, desc: "Una remera que re va con vos", precio: 500, talle: "L", img: '../assets/images/remeraAmarillaHombre.jpg'},
+    {id: 9, nombre: "Remera 3", tipo: "remera", cantidad: 1, desc: "Una remera que re va con vos", precio: 500, talle: "M", img: '../assets/images/remeraBlancaHombre.jpg'},
+    {id: 10, nombre: "Remera 4", tipo: "remera", cantidad: 1, desc: "Una remera que re va con vos", precio: 700, talle: "M", img: '../assets/images/remeraMostazaHombre.jpg'},
+    {id: 11, nombre: "Remera 5", tipo: "remera", cantidad: 1,desc: "Una remera que re va con vos", precio: 700, talle: "S", img: '../assets/images/remeraNegraMujer.jpg'},
+    {id: 12, nombre: "Remera 6", tipo: "remera", cantidad: 1, desc: "Una remera que re va con vos", precio: 700, talle: "S", img: '../assets/images/camisaCelesteHombre.jpg'},
+]
